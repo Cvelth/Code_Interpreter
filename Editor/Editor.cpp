e19062fe51ce623a0f4404e18dfee1c895e56871
cvelth <cvelth.mail@gmail.com>
2017-11-09 00:26:52 +0200

    File opening and saving were implemented.

--- Editor/Editor.cpp
+++ Editor/Editor.cpp
@@ -1,13 +1,83 @@
 #include "Editor.hpp"
 #include "CodeWidget.hpp"
 #include <QLayout>
+#include <QPushButton>
+#include <QMessageBox>
 Editor::Editor(QWidget *parent)	: QWidget(parent) {
+	QHBoxLayout *m_buttons = new QHBoxLayout();
+	QPushButton *m_new = new QPushButton("New");
+	m_buttons->addWidget(m_new, 1);
+	QPushButton *m_open = new QPushButton("Open");
+	m_buttons->addWidget(m_open, 1);
+	QPushButton *m_save = new QPushButton("Save");
+	m_buttons->addWidget(m_save, 1);
+	QPushButton *m_saveAs = new QPushButton("Save as");
+	m_buttons->addWidget(m_saveAs, 1);
+	m_buttons->addStretch(3);
+
 	m_code_editor = new CodeWidget();
 	m_layout = new QVBoxLayout();
+	m_layout->addLayout(m_buttons);
 	m_layout->addWidget(m_code_editor);
 	setLayout(m_layout);
 	resize(1280, 720);
+
+	clear();
+
+	connect(m_new, &QPushButton::clicked, this, &Editor::clear);
+	connect(m_open, &QPushButton::clicked, this, &Editor::open);
+	connect(m_save, &QPushButton::clicked, this, &Editor::save);
+	connect(m_saveAs, &QPushButton::clicked, this, &Editor::saveAs);
 }
 Editor::~Editor() {
+	delete m_new;
+	//delete m_open;
+	//delete m_save;
+	//delete m_saveAs;
+
 	delete m_code_editor;
+}
+void Editor::clear() {
+	m_current_file = nullptr;
+	m_code_editor->clear();
+}
+#include <QFile>
+#include <QFileDialog>
+void Editor::open() {
+	auto filename = QFileDialog::getOpenFileName(this, tr("Open source file"), "../../",
+												 tr("Perl source files (*.pl);;C++ source files (*.hpp, *.cpp);;"
+													"C source files (*.h *.c);;Assembler source files (*.asm, *.s)"));
+	m_current_file = new QFile(filename);
+	if (!m_current_file->open(QIODevice::ReadOnly))
+		QMessageBox(QMessageBox::Warning, "File cannot be opened", "Some error occured during file openning process", 
+					QMessageBox::NoButton, this).exec();
+	else
+		m_code_editor->document()->setPlainText(m_current_file->readAll());
+}
+void Editor::save() {
+	if (m_current_file) {
+		if (!m_current_file->open(QIODevice::WriteOnly))
+			QMessageBox(QMessageBox::Warning, "File cannot be opened", "Some error occured during file openning process",
+						QMessageBox::NoButton, this).exec();
+		else {
+			m_current_file->write(m_code_editor->document()->toPlainText().toUtf8());
+			m_current_file->flush();
+			m_current_file->close();
+		}
+	} else
+		saveAs();
+}
+void Editor::saveAs() {
+	auto filename = QFileDialog::getSaveFileName(this, tr("Save source file"), "../../",
+												 tr("Perl source files (*.pl);;C++ source files (*.hpp, *.cpp);;"
+													"C source files (*.h *.c);;Assembler source files (*.asm, *.s)"));
+	m_current_file = new QFile(filename);
+	if (!m_current_file->open(QIODevice::WriteOnly))
+		QMessageBox(QMessageBox::Warning, "File cannot be opened", "Some error occured during file openning process",
+					QMessageBox::NoButton, this).exec();
+	else {
+		m_current_file->write(m_code_editor->document()->toPlainText().toUtf8());
+		m_current_file->flush();
+		m_current_file->close();
+	}
 }
--- Editor/Editor.hpp
+++ Editor/Editor.hpp
@@ -2,12 +2,27 @@
 #include <QWidget>
 class CodeWidget;
 class QVBoxLayout;
+class QHBoxLayout;
+class QPushButton;
+class QFile;
 class Editor : public QWidget {
 	Q_OBJECT
 private:
 	CodeWidget *m_code_editor;
 	QVBoxLayout *m_layout;
+	QHBoxLayout *m_buttons;
+	QPushButton *m_new;
+	QPushButton *m_open;
+	QPushButton *m_save;
+	QPushButton *m_saveAs;
+
+	QFile *m_current_file;
 public:
 	Editor(QWidget *parent = Q_NULLPTR);
 	~Editor();
+private slots:
+	void clear();
+	void open();
+	void save();
+	void saveAs();
 };
