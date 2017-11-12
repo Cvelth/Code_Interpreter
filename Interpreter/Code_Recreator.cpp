8f345d435e5a6eabeb0cc237d544d000a8bcbf4c
cvelth <cvelth.mail@gmail.com>
2017-11-12 22:04:49 +0200

    Test2 was passed.

--- Interpreter/Code_Recreator.cpp
+++ Interpreter/Code_Recreator.cpp
@@ -5,6 +5,7 @@ size_t variable_index = 0;
 std::string const sh("\t");
 std::map<std::string, size_t> constants;
 std::map<std::string, size_t> inner_constants;
+std::string current_package;
 std::ostream& operator<<(std::ostream &s, std::set<Node> const& set) {
 	for (auto it : set) {
 		if (it.type == TokenType::int_literal) {
@@ -22,22 +23,111 @@ std::ostream& operator<<(std::ostream &s, std::set<Node> const& set) {
 	}
 	return s;
 }
+void insert_parameter(std::ostream &s, Node const& node, size_t &r) {
+	if (node.type == TokenType::binary_operator && node.name == ",") {
+		insert_parameter(s, *node.left, r);
+		insert_parameter(s, *node.right, r);
+	} else if (node.type == TokenType::binary_operator && node.name == "=>") {
+		s << sh << "ldr r" << r++ << ", =" << "_constant_" << constants[node.right->name] << '\n';
+	}
+}
+void string_concat(std::ostream &s, Node const& node, size_t l) {
+	inner_constants.insert(std::make_pair("%s", variable_index++));
+	if (node.left->type == TokenType::binary_operator && node.left->name == ".")
+		string_concat(s, *node.left, l + 1);
+	if (node.right->type == TokenType::binary_operator && node.right->name == ".")
+		string_concat(s, *node.right, l + 1);
+	if (node.left->type == TokenType::string_literal) {
+		inner_constants.insert(std::make_pair("\"\"", variable_index++));
+		s << "\n" << sh << "mov r0, =" << "_constant_" << constants[node.left->name] << '\n'
+			<< sh << "mov r2, =" << "_inner_constant_" << inner_constants["\"\""] << '\n'
+			<< sh << "mov r4, #128\n"
+			<< sh << "loop_l_" << l << ":\n"
+			<< sh << sh << "ldrb r3, [r0], #1\n"
+			<< sh << sh << "strb r3, [r2], #1\n"
+			<< sh << sh << "subs r4, #1\n"
+			<< sh << "bne loop_l_" << l << "\n";
+	}
+	if (node.right->type == TokenType::string_literal) {
+		inner_constants.insert(std::make_pair("\"\"", variable_index++));
+		s << "\n" << sh << "mov r0, =" << "_constant_" << constants[node.right->name] << '\n'
+			<< sh << "mov r2, =" << "_inner_constant_" << inner_constants["\"\""] << '\n'
+			<< sh << "mov r4, #128\n"
+			<< sh << "loop_l_" << l << ":\n"
+			<< sh << sh << "ldrb r3, [r0], #1\n"
+			<< sh << sh << "strb r3, [r2], #1\n"
+			<< sh << sh << "subs r4, #1\n"
+			<< sh << "bne loop_l_" << l << "\n";
+	}
+}
 std::ostream& operator<<(std::ostream &s, Node const& node) {
 	if (node.type == TokenType::semicolon) {
-		if (node.left) s << *node.left;
-		if (node.left) s << "\n";
+		if (node.left) s << *node.left << "\n";
 		if (node.right) s << *node.right;
-		if (node.right) s << "\n";
-	}  else if (node.type == TokenType::reserved_word) {
+	} else if (node.type == TokenType::binary_operator) {
+		if (node.name == ".") {
+			string_concat(s, node, 0);
+		}
+	} else if (node.type == TokenType::reserved_word) {
 		if (node.name == "print") {
 			if (node.right->type == TokenType::string_literal) {
 				inner_constants.insert(std::make_pair("%s", variable_index++));
 				s << sh << "mov r0, =" << "_inner_constant_" << inner_constants["%s"]
 					<< "\n" << sh << "mov r1, =_constant_" << constants[node.right->name]
 					<< "\n" << sh << "bl printf\n";
+			} else if (node.right->type == TokenType::binary_operator && node.right->name == ".") {
+				s << *node.right;
 			}
+		} else if (node.name == "package") {
+			s << "@package " << current_package << " start here.\n";
+		} else if (node.name == "sub") {
+			s << ".extern " << current_package << "_" << node.left->name << '\n';
+			s << current_package << "_" << node.left->name << ":\n"
+				<< sh << "@Subroutine arguments:\n"
+				<< sh << sh << "@r0 - " << current_package << "-pointer\n"
+				<< sh << sh << "@r1 - pointer to the argument list (@)\n"
+				<< sh << "push {r4, lr}\n"
+				<< *node.right
+				<< sh << "pop {r4, pc}\n";
+		} else if (node.name == "my") {
+			if (node.right->type == TokenType::variable_name)
+				s << '\n' << sh << "ldr r0, " << node.right->name << "_var\n"
+					<< sh << "str r0, " << node.left->name << "_var\n";
+			else if (node.right->type == TokenType::reserved_word && node.right->name == "shift") {
+				inner_constants.insert(std::make_pair(current_package, variable_index++));
+				s << '\n' << sh << "ldr r0, " << inner_constants[current_package] << "_var\n"
+					<< sh << "str r0, " << node.left->name << "_var";
+			} else if (node.right->type == TokenType::bracket && node.right->name == "{}") {
+				s << '\n' << sh << "ldr r0, " << (node.right->right->name == "_" ? "r1" : ("=" + node.right->right->name + "_list")) << '\n'
+					<< sh << "str r0, " << node.left->name << "_var";
+			} else if (node.right->type == TokenType::binary_operator && node.right->name == "->") {
+				size_t r = 0;
+				insert_parameter(s, *node.right->right->right, r);
+				s << sh << "bl " << node.right->left->name << "_" << node.right->right->name << "\n";
+				s << sh << "str r0, =" << node.left->name << "_var";
+			}
+		} else if (node.name == "bless") {
+			s << '\n' << sh << "ldr r0, =" << node.right->name << "_var\n"
+				<< sh << "str r0, " << node.left->name << "_var\n";
+		} else if (node.name == "return") {
+			if (node.right->type == TokenType::variable_name)
+				s << sh << "ldr r0, " << node.right->name << "_var";
+			else if (node.right->type == TokenType::binary_operator && node.right->name == "->" && node.right->right->name == "{}" && node.right->right->right->type == TokenType::type_name) {
+				s << '\n' << sh << "ldr r1, " << node.right->left->name << "_var\n";
+				s << sh << "ldr r1, =" << node.right->right->right->name << "_var\n";
+				s << sh << "add r1, r2\n" << sh << "mov r0, =r1";
+			}
+		} else if (node.name == "use") {
+			if (node.left->name == "lib")
+				s << ".extern '" << node.right->name << "'";
+			else
+				s << ".extern " << node.left->name;
 		}
-	}
+	} else if (node.type == TokenType::int_literal && node.name == "1") {
+		s << "@package " << current_package << " ends here.";
+		current_package = "";
+	} else if (node.type == TokenType::bracket && node.name == "{}")
+		s << *node.right;
 	return s;
 }
 std::string code_recreation(Syntax const& syntax, bool semantic_result, bool print_comments) {
@@ -65,15 +155,21 @@ std::string code_recreation(Syntax const& syntax, bool semantic_result, bool pri
 			ret << "\n@Constants used in the file:\n";
 		ret << syntax.constants;
 	}
+
 	ret << "\n.text"
-		<< "\n.extern printf"
-		<< "\n.global main\n\n"
-		<< "main:\n"
-		<< sh << "push {ip, lr}\n\n";
+		<< "\n.extern printf\n";
+	if (syntax.graph->left->type == TokenType::reserved_word && syntax.graph->left->name == "package") {
+		current_package = syntax.graph->left->right->name;
+		ret << *syntax.graph;
+	} else {
+		ret << ".global main\n\n"
+			<< "main:\n"
+			<< sh << "push {ip, lr}\n\n";
 
-	ret << *syntax.graph;
+		ret << *syntax.graph;
 
-	ret << sh << "pop {ip, pc}\n";
+		ret << sh << "pop {ip, pc}\n";
+	}
 
 	if (!inner_constants.empty()) {
 		ret << "\n.data\n";
