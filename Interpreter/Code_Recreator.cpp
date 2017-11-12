484fd14bcffc2537e28d2861856c6cb309f303db
cvelth <cvelth.mail@gmail.com>
2017-11-12 18:54:07 +0200

    std::stringstream is now used.

--- Interpreter/Code_Recreator.cpp
+++ Interpreter/Code_Recreator.cpp
@@ -1,7 +1,22 @@
 #include "Interpreter.hpp"
+#include <sstream>
+std::ostream& operator<<(std::ostream &s, std::set<Node> const& set) {
+
+	return s;
+}
+std::ostream& operator<<(std::ostream &s, Node const& set) {
+
+	return s;
+}
 std::string code_recreation(Syntax const& syntax, bool semantic_result) {
 	if (!semantic_result)
 		throw std::exception("Semantic_analysis was unsuccessful. Terminating...");
 
-	return std::string();
+	std::stringstream ret;
+
+	ret << "@Variables used in the file:\n";
+	ret << syntax.variables;
+	ret << *syntax.graph;
+
+	return ret.str();
 }
