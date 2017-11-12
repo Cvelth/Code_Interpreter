afc82b09ce73b0f5e568ceced7e0f5ca89f6f62c
cvelth <cvelth.mail@gmail.com>
2017-11-12 19:18:03 +0200

    Constants and variables are now returned properly.

--- Interpreter/Code_Recreator.cpp
+++ Interpreter/Code_Recreator.cpp
@@ -1,7 +1,19 @@
 #include "Interpreter.hpp"
 #include <sstream>
+size_t variable_index = 0;
 std::ostream& operator<<(std::ostream &s, std::set<Node> const& set) {
-
+	for (auto it : set) {
+		if (it.type == TokenType::int_literal)
+			s << "_inner_name_" << variable_index++ << ": \t.word\t" << it.name << '\n';
+		else if (it.type == TokenType::string_literal)
+			s << "_inner_name_" << variable_index++ << ": \t.asciz\t\"" << it.name << "\"\n";
+		else if (it.type == TokenType::variable_name)
+			s << it.name << "_var:\t.word\t0\n";
+		else if (it.type == TokenType::list_name)
+			s << it.name << "_list:\t.word\t0\n";
+		else if (it.type == TokenType::hash_name)
+			s << it.name << "_hash:\t.word\t0\n";
+	}
 	return s;
 }
 std::ostream& operator<<(std::ostream &s, Node const& set) {
@@ -14,8 +26,14 @@ std::string code_recreation(Syntax const& syntax, bool semantic_result) {
 
 	std::stringstream ret;
 
-	ret << "@Variables used in the file:\n";
-	ret << syntax.variables;
+	if (!syntax.variables.empty()) {
+		ret << "@Variables used in the file:\n";
+		ret << syntax.variables;
+	}
+	if (!syntax.constants.empty()) {
+		ret << "@Constants used in the file:\n";
+		ret << syntax.constants;
+	}
 	ret << *syntax.graph;
 
 	return ret.str();
